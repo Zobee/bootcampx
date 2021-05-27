7d5d09f2a0254b72cea13f0634dd07f81b92dc7d
Zobee <zohebboga@hotmail.com>
2021-05-27 00:48:20 +0000

    Updated students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const args = process.argv.slice(2)
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
 pool.query(`
 SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${args[0]}%'
-LIMIT ${args[1] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${cohortName}%`, limit])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const arg = process.argv[2]
+const cohortName = process.argv[2] || 'JUL02';
 
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,8 +15,8 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${arg || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`, [cohortName])
 .then(res => console.log(res.rows))
 .catch(err => console.log(err))
